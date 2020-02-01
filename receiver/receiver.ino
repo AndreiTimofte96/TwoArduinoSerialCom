fe3d20200e80932a4a2e4725af707aa63892a009
Andrei Teodor Timofte <Andrei.timofte@endava.com>
2020-02-01 19:58:31 +0200

    add udp packets

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,23 +1,80 @@
+// 32,16,8,0,Ana are mere si
+enum blockInformation {
+  BLOCK_SIZE = 32,
+  BLOCK_HEADER_SIZE = 16,
+  BLOCK_BODY_SIZE = 16
+};
+
+enum connectionStatus {
+  READY,
+  RUNNING,
+};
+
+struct Connection {
+  connectionStatus status;
+} connection;
+
+struct MyUdpPacket {
+  int pSize;
+  int bLength;
+  int bNumber;
+  int bOffset;
+  char* bData;
+} myUdpPacket;
+
+char dataToReceive[1000];
 
 void setup() {
-  // Begin the Serial at 9600 Baud
   Serial.begin(9600);
   Serial.setTimeout(500);
+  Serial.flush();
+
+  connection.status = READY;
 }
 
-char finalResult[1000];
-void loop() {
-  if (Serial.available()) {
-    int dataLength = Serial.parseInt();
-    Serial.print("DATA LENGTH: ");
-    Serial.println(dataLength);
-    char data[dataLength];
+void formatReceiveData(MyUdpPacket& myUdpPacket, char* bData) {
+  char* pch;
+  pch = strtok(bData, ",");
+  myUdpPacket.pSize = atoi(pch);
 
-    Serial.readBytes(data, dataLength);
-    Serial.print("RECEIVED: ");
-    // for (int index = 0; index < dataLength; index++) {
-    //   Serial.print(data[index]);
-    // }
-    Serial.println(data);
+  pch = strtok(NULL, ",");
+  myUdpPacket.bLength = atoi(pch);
+
+  pch = strtok(NULL, ",");
+  myUdpPacket.bNumber = atoi(pch);
+
+  pch = strtok(NULL, ",");
+  myUdpPacket.bOffset = atoi(pch);
+
+  pch = strtok(NULL, ",");
+  myUdpPacket.bData = pch;
+
+  strcat(dataToReceive, myUdpPacket.bData);
+}
+
+void receiveData(char* dataToReceive) {
+  int bDataLength = BLOCK_SIZE;
+  char bData[bDataLength];
+  memset(bData, '\0', sizeof(char) * bDataLength);
+  Serial.readBytes(bData, bDataLength);
+  formatReceiveData(myUdpPacket, bData);
+
+  while (myUdpPacket.bOffset + 1 < myUdpPacket.bNumber) {
+    while (!Serial.available())
+      ;
+    memset(bData, '\0', sizeof(char) * bDataLength);
+    Serial.readBytes(bData, bDataLength);
+    formatReceiveData(myUdpPacket, bData);
+  }
+}
+
+void loop() {
+  if (connection.status == READY && Serial.available()) {
+    connection.status = RUNNING;
+    memset(dataToReceive, '\0', sizeof(char) * 1000);
+    receiveData(dataToReceive);
+    Serial.println("RECEIVED DATA:");
+    Serial.println(dataToReceive);
+    connection.status = READY;
   }
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -1,32 +1,74 @@
+char dataToSend[] = "Ana are mere si gutui si tata are pere si eu vreau sa le mananc pe toate. Unu doi trei patru cinci sase sapte opt noua zece.";
+// 32,16,8,0,Ana are mere si
+
+struct MyUdpPacket {
+  int pSize;
+  int bLength;
+  int bNumber;
+  int bOffset;
+  char *bData;
+} myUdpPacket;
+
+enum connectionStatus {
+  READY,
+  RUNNING,
+};
+
+struct Connection {
+  connectionStatus status;
+} connection;
 
 void setup() {
-  // Begin the Serial at 9600 Baud
   Serial.begin(9600);
   Serial.setTimeout(500);
+
+  connection.status = READY;
+  myUdpPacket.pSize = 32;
+  myUdpPacket.bLength = 16;
+  delay(10000);
 }
 
-String data = "Ana are mere si gutui si tata are pere si eu vreau sa le mananc pe toate.";
+void addNumberToCharArray(int number, char *str) {
+  char aux[100];
+  sprintf(aux, "%d,", number);
+  strcat(str, aux);
+}
+void formatSendData(MyUdpPacket &myUdpPacket, char *packet) {
+  addNumberToCharArray(myUdpPacket.pSize, packet);
+  addNumberToCharArray(myUdpPacket.bLength, packet);
+  addNumberToCharArray(myUdpPacket.bNumber, packet);
+  addNumberToCharArray(myUdpPacket.bOffset, packet);
+  strcat(packet, myUdpPacket.bData);
+}
 
-bool sendData(String data) {
-  int blockSize = 8;
-  char block[blockSize];
+bool sendData(char *dataToSend) {
+  myUdpPacket.bNumber = (strlen(dataToSend) / myUdpPacket.bLength) + 1;
 
-  for (int index = 0; index < data.length(); index++) {
-    for (int blockIndex = 0; blockIndex < blockSize; blockIndex++) {
-      block[blockIndex] = data[index++];
-    }
-    index--;
-    int iBlockLength = sizeof(block);
-    char sendData[100];
-    snprintf(sendData, 100, "%d%s", iBlockLength, block);
-    int iSendDataLength = strlen(sendData);
+  int bLength = myUdpPacket.bLength;
+  for (int blockIndex = 0; blockIndex < myUdpPacket.bNumber; blockIndex++) {
+    myUdpPacket.bOffset = blockIndex;
+    myUdpPacket.bData = (char *)malloc(sizeof(char) * myUdpPacket.bLength + 1);
+    memset(myUdpPacket.bData, '\0', sizeof(char) * myUdpPacket.bLength + 1);
 
-    Serial.write(sendData, iSendDataLength);
-    block[0] = '\0';
+    for (int dataIndex = blockIndex * bLength; dataIndex < blockIndex * bLength + bLength; dataIndex++) {
+      int bDataLength = strlen(myUdpPacket.bData);
+      myUdpPacket.bData[bDataLength] = dataToSend[dataIndex];
+    }  // formam blocurile de cate 12
+    // myUdpPacket.bData[strlen(myUdpPacket.bData) - 1] = '\0';
+
+    char packet[myUdpPacket.pSize];
+    memset(packet, '\0', sizeof(char) * myUdpPacket.pSize);
+    formatSendData(myUdpPacket, packet);
+    Serial.write(packet, strlen(packet));
+    delay(1000);
   }
 }
 
 void loop() {
-  sendData(data);
+  if (connection.status == READY) {
+    connection.status = RUNNING;
+    sendData(dataToSend);
+    connection.status = READY;
+  }
   delay(5000);
 }
