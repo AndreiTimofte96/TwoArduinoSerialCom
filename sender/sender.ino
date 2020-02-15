434897bfd89893b50cc82683acd623fad787787c
timofteandrei <andrei.timofte@feel-it-services.com>
2020-02-15 23:20:02 +0200

    add client option

--- sender/sender.ino
+++ sender/sender.ino
@@ -4,21 +4,36 @@
 UdpProtocol udpProtocol;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(500);
+    Serial.begin(9600);
+    Serial.setTimeout(500);
 }
 
 char dataToSend1[] = "Ana are mere si gutui si tata are pere si eu vreau sa le mananc pe toate. Unu doi trei patru cinci sase sapte opt noua zece.";
-char dataToSend2[] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
+char dataToSend2[] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
+
+char option[10];
+
+char dataToSend[250];
 
 void loop() {
-  if (!udpProtocol.arduinoAcceptConnection()) {
-    udpProtocol.printLastError();
-  }
+    //   if (!udpProtocol.arduinoAcceptConnection()) {
+    //     udpProtocol.printLastError();
+    //   }
+    if (!udpProtocol.udpRead(option)) {
+        udpProtocol.printLastError();
+    }
 
-  if (!udpProtocol.udpWrite(dataToSend2)) {
-    udpProtocol.printLastError();
-  }
+    if (strcmp(option, "1") == 0) {
+        strcpy(dataToSend, dataToSend1);
+    }
 
-  udpProtocol.arduinoServerClose();
+    if (strcmp(option, "2") == 0) {
+        strcpy(dataToSend, dataToSend2);
+    }
+
+    if (!udpProtocol.udpWrite(dataToSend)) {
+        udpProtocol.printLastError();
+    }
+
+    udpProtocol.arduinoServerClose();
 }
--- udpProtocol/udpProtocol.cpp
+++ udpProtocol/udpProtocol.cpp
@@ -1,214 +1,216 @@
 #include "udpProtocol.h"
 
 UdpProtocol::UdpProtocol() {
-  connection.setStatus(Connection::DISCONNECTED);
-  packetWrite.pSize = UdpPacket::BLOCK_SIZE;
-  packetWrite.bLength = UdpPacket::BLOCK_BODY_SIZE;
+    // connection.setStatus(Connection::DISCONNECTED);
+    connection.setStatus(Connection::CONNECTED);
+
+    packetWrite.pSize = UdpPacket::BLOCK_SIZE;
+    packetWrite.bLength = UdpPacket::BLOCK_BODY_SIZE;
 }
 char CONNECTED_TEXT[] = "CONNECTED";
 
 ///////////////
 void UdpProtocol::waitRead() {
-  while (!Serial.available())
-    ;
+    while (!Serial.available())
+        ;
 }
 
 void UdpProtocol::serialFlush() {
-  byte b;
-  while (Serial.readBytes(&b, 1) > 0)
-    ;
+    byte b;
+    while (Serial.readBytes(&b, 1) > 0)
+        ;
 }
 
 void UdpProtocol::println(char *data) {
-  Serial.println(data);
+    Serial.println(data);
 }
 
 void UdpProtocol::println(int data) {
-  Serial.println(data);
+    Serial.println(data);
 }
 ///////////////
 
 //////////////////
 
 void UdpProtocol::printLastError() {
-  Serial.print("EROARE: ");
-  Serial.println(error.getError());
+    Serial.print("EROARE: ");
+    Serial.println(error.getError());
 }
 ////////////////////
 
 ////////////////////
 bool UdpProtocol::arduinoAcceptConnection() {
-  if (connection.getStatus() == Connection::DISCONNECTED) {
-    Serial.println();
-    Serial.println("WAITING FOR CONNECTION");
+    if (connection.getStatus() == Connection::DISCONNECTED) {
+        Serial.println();
+        Serial.println("WAITING FOR CONNECTION");
 
-    int connectedLength = strlen(CONNECTED_TEXT);
-    char connected[connectedLength];
+        int connectedLength = strlen(CONNECTED_TEXT);
+        char connected[connectedLength];
 
-    waitRead();
-    Serial.readBytes(connected, connectedLength);
+        waitRead();
+        Serial.readBytes(connected, connectedLength);
 
-    if (strcmp(connected, CONNECTED_TEXT) == 0) {
-      Serial.write(CONNECTED_TEXT, connectedLength);
-      connection.setStatus(Connection::CONNECTED);
-      return true;
+        if (strcmp(connected, CONNECTED_TEXT) == 0) {
+            Serial.write(CONNECTED_TEXT, connectedLength);
+            connection.setStatus(Connection::CONNECTED);
+            return true;
+        }
+
+        connection.setStatus(Connection::DISCONNECTED);
+        error.setError(Error::CONNECTION_ERROR);
+        Serial.write(error.getError(), strlen(error.getError()));
+
+        return false;
     }
-
     connection.setStatus(Connection::DISCONNECTED);
     error.setError(Error::CONNECTION_ERROR);
-    Serial.write(error.getError(), strlen(error.getError()));
 
     return false;
-  }
-  connection.setStatus(Connection::DISCONNECTED);
-  error.setError(Error::CONNECTION_ERROR);
-
-  return false;
 }
 
 bool UdpProtocol::arduinoConnect() {
-  if (connection.getStatus() == Connection::DISCONNECTED) {
-    Serial.write(CONNECTED_TEXT, strlen(CONNECTED_TEXT));
-    waitRead();
+    if (connection.getStatus() == Connection::DISCONNECTED) {
+        Serial.write(CONNECTED_TEXT, strlen(CONNECTED_TEXT));
+        waitRead();
 
-    int connectedLength = strlen(CONNECTED_TEXT);
-    char connected[connectedLength];
-    Serial.readBytes(connected, connectedLength);
-    Serial.println(connected);
+        int connectedLength = strlen(CONNECTED_TEXT);
+        char connected[connectedLength];
+        Serial.readBytes(connected, connectedLength);
+        Serial.println(connected);
 
-    if (strcmp(connected, CONNECTED_TEXT) == 0) {
-      connection.setStatus(Connection::CONNECTED);
+        if (strcmp(connected, CONNECTED_TEXT) == 0) {
+            connection.setStatus(Connection::CONNECTED);
 
-      return true;
-    };
-  }
+            return true;
+        };
+    }
 
-  connection.setStatus(Connection::DISCONNECTED);
+    connection.setStatus(Connection::DISCONNECTED);
 
-  error.setError(Error::CONNECTION_ERROR);
-  return false;
+    error.setError(Error::CONNECTION_ERROR);
+    return false;
 }
 
 bool UdpProtocol::arduinoServerClose() {
-  while (1)
-    ;
-  serialFlush();
-  connection.setStatus(Connection::IDLE);
+    while (1)
+        ;
+    serialFlush();
+    connection.setStatus(Connection::IDLE);
 }
 
 bool UdpProtocol::arduinoClose() {
-  while (1)
-    ;
-  connection.setStatus(Connection::IDLE);
+    while (1)
+        ;
+    connection.setStatus(Connection::IDLE);
 }
 ///////////////////////
 
 ////////////////
 void UdpProtocol::addNumberToCharArray(int number, char *str) {
-  char aux[100];
-  sprintf(aux, "%d,", number);
-  strcat(str, aux);
+    char aux[100];
+    sprintf(aux, "%d,", number);
+    strcat(str, aux);
 }
 
 void UdpProtocol::addOffsetToCharArray(char *str) {
-  int strLength = strlen(str);
-  for (int index = strLength; index < UdpPacket::BLOCK_HEADER_SIZE; index++) {
-    str[index] = ',';
-  }
+    int strLength = strlen(str);
+    for (int index = strLength; index < UdpPacket::BLOCK_HEADER_SIZE; index++) {
+        str[index] = ',';
+    }
 }
 
 void UdpProtocol::formatSendData(char *packet) {
-  addNumberToCharArray(packetWrite.pSize, packet);
-  addNumberToCharArray(packetWrite.bLength, packet);
-  addNumberToCharArray(packetWrite.bNumber, packet);
-  addNumberToCharArray(packetWrite.bOffset, packet);
-  addOffsetToCharArray(packet);
-  strcat(packet, packetWrite.bData);
+    addNumberToCharArray(packetWrite.pSize, packet);
+    addNumberToCharArray(packetWrite.bLength, packet);
+    addNumberToCharArray(packetWrite.bNumber, packet);
+    addNumberToCharArray(packetWrite.bOffset, packet);
+    addOffsetToCharArray(packet);
+    strcat(packet, packetWrite.bData);
 }
 
 void UdpProtocol::sendData(char *dataToSend) {
-  packetWrite.bNumber = (strlen(dataToSend) / packetWrite.bLength) + 1;
-  int remainder = strlen(dataToSend) % packetWrite.bLength;
-  int bLength = packetWrite.bLength;
-  int blockLength = bLength;
-  for (int blockIndex = 0; blockIndex < packetWrite.bNumber; blockIndex++) {
-    if (blockIndex == packetWrite.bNumber - 1) {
-      packetWrite.bLength = remainder;
-      blockLength = remainder;
-    }
-    packetWrite.bOffset = blockIndex;
-    packetWrite.bData = (char *)malloc(sizeof(char) * packetWrite.bLength + 1);
-    memset(packetWrite.bData, '\0', sizeof(char) * packetWrite.bLength + 1);
+    packetWrite.bNumber = (strlen(dataToSend) / packetWrite.bLength) + 1;
+    int remainder = strlen(dataToSend) % packetWrite.bLength;
+    int bLength = packetWrite.bLength;
+    int blockLength = bLength;
+    for (int blockIndex = 0; blockIndex < packetWrite.bNumber; blockIndex++) {
+        if (blockIndex == packetWrite.bNumber - 1) {
+            packetWrite.bLength = remainder;
+            blockLength = remainder;
+        }
+        packetWrite.bOffset = blockIndex;
+        packetWrite.bData = (char *)malloc(sizeof(char) * packetWrite.bLength + 1);
+        memset(packetWrite.bData, '\0', sizeof(char) * packetWrite.bLength + 1);
 
-    for (int dataIndex = blockIndex * bLength; dataIndex < blockIndex * bLength + blockLength; dataIndex++) {
-      int bDataLength = strlen(packetWrite.bData);
-      packetWrite.bData[bDataLength] = dataToSend[dataIndex];
+        for (int dataIndex = blockIndex * bLength; dataIndex < blockIndex * bLength + blockLength; dataIndex++) {
+            int bDataLength = strlen(packetWrite.bData);
+            packetWrite.bData[bDataLength] = dataToSend[dataIndex];
+        }
+        char packet[packetWrite.pSize];
+        memset(packet, '\0', sizeof(char) * packetWrite.pSize);
+        formatSendData(packet);
+        Serial.write(packet, strlen(packet));
     }
-    char packet[packetWrite.pSize];
-    memset(packet, '\0', sizeof(char) * packetWrite.pSize);
-    formatSendData(packet);
-    Serial.write(packet, strlen(packet));
-  }
 }
 
 bool UdpProtocol::udpWrite(char *dataToSend) {
-  if (connection.getStatus() == Connection::CONNECTED) {
-    sendData(dataToSend);
-    return true;
-  }
-  error.setError(Error::WRITE_ERROR);
-  return false;
+    if (connection.getStatus() == Connection::CONNECTED) {
+        sendData(dataToSend);
+        return true;
+    }
+    error.setError(Error::WRITE_ERROR);
+    return false;
 }
 ////////////////////
 
 /////////////////////////
 void UdpProtocol::formatReceiveData(char *bData, char *dataToReceive) {
-  char *pch;
-  pch = strtok(bData, ",");
-  packetRead.pSize = atoi(pch);
+    char *pch;
+    pch = strtok(bData, ",");
+    packetRead.pSize = atoi(pch);
 
-  pch = strtok(NULL, ",");
-  packetRead.bLength = atoi(pch);
+    pch = strtok(NULL, ",");
+    packetRead.bLength = atoi(pch);
 
-  pch = strtok(NULL, ",");
-  packetRead.bNumber = atoi(pch);
+    pch = strtok(NULL, ",");
+    packetRead.bNumber = atoi(pch);
 
-  pch = strtok(NULL, ",");
-  packetRead.bOffset = atoi(pch);
+    pch = strtok(NULL, ",");
+    packetRead.bOffset = atoi(pch);
 
-  pch = strtok(NULL, ",");
-  packetRead.bData = pch;
+    pch = strtok(NULL, ",");
+    packetRead.bData = pch;
 
-  packetRead.bData[packetRead.bLength] = '\0';
-  strcat(dataToReceive, packetRead.bData);
+    packetRead.bData[packetRead.bLength] = '\0';
+    strcat(dataToReceive, packetRead.bData);
 }
 
 void UdpProtocol::receiveData(char *dataToReceive) {
-  int bDataLength = UdpPacket::BLOCK_SIZE;
-  char bData[bDataLength];
+    int bDataLength = UdpPacket::BLOCK_SIZE;
+    char bData[bDataLength];
 
-  // serialFlush();
-  waitRead();
-  memset(bData, '\0', sizeof(char) * bDataLength);
-  Serial.readBytes(bData, bDataLength);
-  formatReceiveData(bData, dataToReceive);
-
-  while (packetRead.bOffset + 1 < packetRead.bNumber) {
+    // serialFlush();
     waitRead();
     memset(bData, '\0', sizeof(char) * bDataLength);
     Serial.readBytes(bData, bDataLength);
     formatReceiveData(bData, dataToReceive);
-  }
+
+    while (packetRead.bOffset + 1 < packetRead.bNumber) {
+        waitRead();
+        memset(bData, '\0', sizeof(char) * bDataLength);
+        Serial.readBytes(bData, bDataLength);
+        formatReceiveData(bData, dataToReceive);
+    }
 }
 
 bool UdpProtocol::udpRead(char *dataToReceive) {
-  if (connection.getStatus() == Connection::CONNECTED) {
-    memset(dataToReceive, '\0', sizeof(char) * sizeof(dataToReceive));
-    receiveData(dataToReceive);
-    return true;
-  }
-  error.setError(Error::READ_ERROR);
+    if (connection.getStatus() == Connection::CONNECTED) {
+        memset(dataToReceive, '\0', sizeof(char) * sizeof(dataToReceive));
+        receiveData(dataToReceive);
+        return true;
+    }
+    error.setError(Error::READ_ERROR);
 
-  return false;
+    return false;
 }
 //////////////
