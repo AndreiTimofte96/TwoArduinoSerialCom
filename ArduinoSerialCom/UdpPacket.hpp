db0406c505c8f6787c45e6a3b13993b57266bb1a
AndreiTimofte96 <andrei.timofte96@gmail.com>
2020-05-19 20:54:38 +0300

    adapt UdpPacket values

--- ArduinoSerialCom/UdpPacket.hpp
+++ ArduinoSerialCom/UdpPacket.hpp
@@ -3,19 +3,19 @@ class UdpPacket {
  public:
   UdpPacket();
 
-  int pSize;
-  int bLength;
-  int bNumber;
-  int bOffset;
-  int checkSum1;
-  int checkSum2;
-  int fromUAID;
-  int toUAID;
+  int pSize;      // 2
+  int bLength;    //2,  length of a block: eg: 16 bytes or if it last block != 16 bytes
+  int bNumber;    //3, number of blocks of the packet
+  int bOffset;    //3 , offset of block x / bNumber
+  int checkSum1;  //3,
+  int checkSum2;  //3
+  int fromUAID;   // 4
+  int toUAID;     // 4
   char *bData;
 
   enum blockInformation {
-    BLOCK_SIZE = 49,
-    BLOCK_HEADER_SIZE = 33,
+    BLOCK_SIZE = 48,
+    BLOCK_HEADER_SIZE = 32,
     BLOCK_BODY_SIZE = 16
   };
 };
